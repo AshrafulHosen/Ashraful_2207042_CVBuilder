bade048ba05406a4c2e098700d66916b6c3e3169
AsharfulHosen <ashrafulaliff7@gamil.com>
2025-11-10 22:43:57 +0600

    Initializing repo

---
+++ src/main/java/com/example/ashraful_2207042_cvbuilder/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.ashraful_2207042_cvbuilder;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/com/example/ashraful_2207042_cvbuilder/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.ashraful_2207042_cvbuilder;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
